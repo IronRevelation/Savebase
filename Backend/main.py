f9f12dcca02ba4d94036be755e94649349979f9e
IronRevelation <69801595+IronRevelation@users.noreply.github.com>
2021-03-17 16:07:09 +0100

    added database

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,15 @@
 from flask import Flask
 from utils import get_frontend_path
+from flask_pymongo import PyMongo
+import os
+from dotenv import load_dotenv
 app = Flask(__name__, static_folder=get_frontend_path(""), static_url_path='')
+
+load_dotenv()
+
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+mongo = PyMongo(app)
+users = mongo.db.Users
 
 @app.route('/')
 def home():
